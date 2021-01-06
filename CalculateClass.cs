a0d284cde08ad42513e112005fccd24c5e43df1d
adjuc <anders@codeimprover.com>
2021-01-06 15:49:17 +0100

    c-2021-01-06-1549

---
+++ CalculateClass.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace lowq
+{
+    internal class Calculate
+    {
+        public int CalculateSumOf8(int v1, int v2, int v3, int v4, int v5, int v6, int v7, int v8)
+        {
+            return(v1+v2+v3+v4+v5+v6+v7+v8);
+        }
+    }
+}
