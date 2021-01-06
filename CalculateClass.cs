fae7fff807b99fef24d6b9b5c2d78fbd3098b73d
adjuc <anders@codeimprover.com>
2021-01-06 15:59:34 +0100

    C-2021-01-05-15.59

--- CalculateClass.cs
+++ CalculateClass.cs
@@ -2,9 +2,9 @@ using System;
 
 namespace lowq
 {
-    internal class Calculate
+    internal class CalculateClass
     {
-        public int CalculateSumOf8(int v1, int v2, int v3, int v4, int v5, int v6, int v7, int v8)
+        public int SumOf8(int v1, int v2, int v3, int v4, int v5, int v6, int v7, int v8)
         {
             return(v1+v2+v3+v4+v5+v6+v7+v8);
         }
--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace lowq
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World from lowq");
+            CalculateClass cs = new CalculateClass();
+            Console.WriteLine("SumOf8() is:" + cs.SumOf8(1,2,3,4,5,6,7,8));
         }
     }
 }
