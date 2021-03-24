d1f074498fd4e9b4736ecffa8a9155fb3b2bc015
adjursaaFoxGmail <72859988+adjursaaFoxGmail@users.noreply.github.com>
2021-03-24 08:26:17 +0100

    Create nyfil24.cs

---
+++ nyfil24.cs
@@ -0,0 +1,16 @@
+namespace lowq
+{
+    public class CalculateClass: ICalculate
+    {
+        int l1, l2, l3, l4, l5, l6, l7, l8, l9;
+        public CalculateClass(int v1, int v2, int v3, int v4, int v5, int v6, int v7, int v8, int v9)
+        {
+            l1 = v1;
+            l2 = v2;
+            l3 = v3;
+            l4 = v4;
+            l5 = v5;
+            l6 = v6;
+            l7 = v7;
+            l8 = v8;
+            l9 = v9;
