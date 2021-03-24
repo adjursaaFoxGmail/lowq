e6554456848fcc9c43902189092cd93de857fee6
adjursaaFoxGmail <72859988+adjursaaFoxGmail@users.noreply.github.com>
2021-03-24 08:16:17 +0100

    Update ICalculate.cs

--- ICalculate.cs
+++ ICalculate.cs
@@ -10,4 +10,14 @@ namespace HelloWorld
         int SumOf9(int v1, int v2, int v3, int v4, int v5, int v6, int v7, int v8, int v9);
             
     }
+    public interface ICalculate
+    {
+        int SumOf9(int v1, int v2, int v3, int v4, int v5, int v6, int v7, int v8, int v9);
+            
+    }
+    public interface ICalculate
+    {
+        int SumOf9(int v1, int v2, int v3, int v4, int v5, int v6, int v7, int v8, int v9);
+            
+    }
 }
